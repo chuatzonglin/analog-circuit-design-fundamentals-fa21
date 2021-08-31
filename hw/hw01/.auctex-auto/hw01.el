9e353f7e43f168d63a70de5156b66ad32141c3a4
Kuroba <Kuroba@Kobra.local>
2021-08-31 17:58:03 +0800

    hw01: q1: completed

---
+++ hw/hw01/.auctex-auto/hw01.el
@@ -0,0 +1,42 @@
+(TeX-add-style-hook
+ "hw01"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "left=0.5in" "right=0.5in" "top=0.5in" "bottom=1in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "float")
+   (LaTeX-add-labels
+    "sec:org7548468"
+    "sec:orgbc40fd9"
+    "sec:org03a3502"
+    "sec:org01f92e7"
+    "sec:orgf8fb051"
+    "sec:org7667dec"
+    "sec:orgc9ac972"))
+ :latex)
+
