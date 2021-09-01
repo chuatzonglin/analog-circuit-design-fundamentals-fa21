811482ecda72bb1704a17c3c126239da935d033f
Kuroba <Kuroba@Kobra.local>
2021-09-01 09:53:39 +0800

    hw01: q3: nmos: completed

--- hw/hw01/.auctex-auto/hw01.el
+++ hw/hw01/.auctex-auto/hw01.el
@@ -3,13 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "left=0.5in" "right=0.5in" "top=0.5in" "bottom=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,15 +31,16 @@
     "geometry"
     "float")
    (LaTeX-add-labels
-    "sec:orgb51baf5"
-    "sec:orga7d3dc1"
-    "sec:orge891bc5"
+    "sec:org9affcb2"
+    "sec:org3732bc0"
+    "sec:org4723046"
     "fig:nmos-testbench"
     "fig:nmos-id-vgs"
     "fig:nmos-ucox-vgs"
-    "sec:orgaf4225b"
-    "sec:org7e27b86"
-    "sec:org6481c7a"
-    "sec:org42fc2a8"))
+    "fig:nmos-sqrt-id-vgs"
+    "sec:org783ffcc"
+    "sec:org2e0d414"
+    "sec:org4c8574b"
+    "sec:orga4a9cd0"))
  :latex)
 
