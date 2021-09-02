3a7c2a5a39dbb7f945e1efc2ec6def618191ace7
Kuroba <Kuroba@Kobra.local>
2021-09-02 11:15:42 +0800

    hw01: q3: pmos: completed

--- hw/hw01/.auctex-auto/hw01.el
+++ hw/hw01/.auctex-auto/hw01.el
@@ -3,13 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "left=0.5in" "right=0.5in" "top=0.5in" "bottom=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,19 +31,23 @@
     "geometry"
     "float")
    (LaTeX-add-labels
-    "sec:org2ba0724"
-    "sec:org150ed51"
-    "sec:orgabf654a"
+    "sec:orgaea26fb"
+    "sec:orgeded328"
+    "sec:org9375af8"
     "fig:nmos-testbench"
     "fig:nmos-id-vgs"
+    "fig:pmos-testbench"
+    "fig:pmos-is-vgd"
     "fig:nmos-ucox-vgs"
     "fig:nmos-sqrt-id-vgs"
-    "sec:orga034705"
+    "fig:pmos-ucox-vgd"
+    "fig:nmos-sqrt-is-vgd"
+    "sec:org6338f52"
     "fig:nmos-testbench-2"
     "fig:nmos-id-vds"
-    "sec:orgb96392b"
-    "sec:org956f36b"
+    "sec:org83713d3"
+    "sec:org1053f6e"
     "fig:small-signal-model"
-    "sec:orgd9598b0"))
+    "sec:org3054950"))
  :latex)
 
