a4615c417545ab5dba7dde62e6d5cae7fb073607
Kuroba <Kuroba@Kobra.local>
2021-09-03 14:38:49 +0800

    hw01:q7:completed

--- hw/hw01/.auctex-auto/hw01.el
+++ hw/hw01/.auctex-auto/hw01.el
@@ -3,13 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "left=0.5in" "right=0.5in" "top=0.5in" "bottom=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,9 +31,9 @@
     "geometry"
     "float")
    (LaTeX-add-labels
-    "sec:org07cf3ba"
-    "sec:orge151912"
-    "sec:org9bc9f36"
+    "sec:org1566dd9"
+    "sec:org0124d81"
+    "sec:org0c9c0e7"
     "fig:nmos-testbench"
     "fig:nmos-id-vgs"
     "fig:pmos-testbench"
@@ -42,17 +42,17 @@
     "fig:nmos-sqrt-id-vgs"
     "fig:pmos-ucox-vgs"
     "fig:nmos-sqrt-is-vgs"
-    "sec:org5ccf270"
+    "sec:org2ebaa55"
     "fig:nmos-testbench-2"
     "fig:nmos-id-vds"
     "fig:pmos-testbench-2"
     "fig:nmos-d-id-id"
     "fig:nmos-d-is-is"
-    "sec:org674837c"
+    "sec:org13acd5e"
     "fig:nmos-d-id-id-vgs"
     "fig:pmos-d-is-is-vgs"
-    "sec:org273d1f9"
+    "sec:orgf432feb"
     "fig:small-signal-model"
-    "sec:orgfdd34d0"))
+    "sec:org984d18d"))
  :latex)
 
