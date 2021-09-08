c6eb91a7f9987ba0e9a2c38bef7795ff1eec1486
TL <Kuroba@Kobra.local>
2021-09-08 23:40:36 +0200

    hw01: minor update

--- hw/hw01/.auctex-auto/hw01.el
+++ hw/hw01/.auctex-auto/hw01.el
@@ -3,13 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "left=0.5in" "right=0.5in" "top=0.5in" "bottom=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,10 +31,9 @@
     "geometry"
     "float")
    (LaTeX-add-labels
-    "sec:orgeba989f"
-    "sec:org525f746"
-    "sec:orgcbbcba2"
-    "sec:org92ebece"
+    "sec:org2869a93"
+    "sec:org6bed224"
+    "sec:org4dd587e"
     "fig:nmos-testbench"
     "fig:nmos-id-vgs"
     "fig:pmos-testbench"
@@ -43,18 +42,18 @@
     "fig:nmos-sqrt-id-vgs"
     "fig:pmos-ucox-vgs"
     "fig:nmos-sqrt-is-vgs"
-    "sec:orgbbcf066"
+    "sec:org2b6165b"
     "fig:nmos-testbench-2"
     "fig:nmos-id-vds"
     "fig:pmos-testbench-2"
     "fig:nmos-d-id-id"
     "fig:nmos-d-is-is"
-    "sec:orgbdcb894"
+    "sec:orgd85e074"
     "fig:nmos-d-id-id-vgs"
     "fig:pmos-d-is-is-vgs"
-    "sec:org704eaea"
+    "sec:orgabba380"
     "fig:small-signal-model"
-    "sec:org6e7a368"
+    "sec:orgc5de167"
     "fig:cir1-vout-cal"
     "fig:cir1-vout-sim-cal"
     "fig:cir1-dvout"
